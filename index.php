ca863b55aef66ed2efdeb7c38cc9464f1353eff7
Brian B <bauma2bm@cmich.edu>
2016-09-13 13:10:12 -0400

    Initial push

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<p><a href="http://portableapps.com/apps/internet/filezilla_portable">Download a portable copy of Filezilla here.</a></p>
+<p><a href="https://filezilla-project.org/">For a full version of Filezilla, click here.</a></p>
+<p><a href="https://the.earth.li/~sgtatham/putty/latest/x86/putty.exe">Grab a copy of PuTTY here.</a></p>
+<p><a href="https://www.sublimetext.com/3">Grad a text editor here.</a></p>
+<p>During our next meeting, we will get everyone logged in, and work on this page.</p>
+</body>
+</html>
